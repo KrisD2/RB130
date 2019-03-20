672d4251cac0630fb8037d1b919491d373bd60ec
Kris Denert <kris.denert@gmail.com>
2019-03-19 22:03:31 -0600

    uploading lesson 1 files

---
+++ lesson_1/todo.rb
@@ -0,0 +1,149 @@
+class Todo
+  DONE_MARKER = 'X'
+  UNDONE_MARKER = ' '
+
+  attr_accessor :title, :description, :done
+
+  def initialize(title, description='')
+    @title = title
+    @description = description
+    @done = false
+  end
+
+  def done!
+    self.done = true
+  end
+
+  def done?
+    done
+  end
+
+  def undone!
+    self.done = false
+  end
+
+  def to_s
+    "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+  end
+end
+
+class TodoList
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+    @todos = []
+  end
+
+  def add(todo)
+    raise TypeError, "Can only add Todo objects" unless todo.class == Todo
+    @todos << todo
+    self.to_s
+  end
+
+  alias_method :<<, :add
+
+  def size
+    @todos.size
+  end
+
+  def first
+    @todos.first
+  end
+
+  def last
+    @todos.last
+  end
+
+  def to_a
+    @todos
+  end
+
+  def done?
+    @todos.all? { |item| item.done? }
+  end
+
+  def item_at(index)
+    @todos.fetch(index)
+  end
+
+  def mark_done_at(index)
+    @todos.fetch(index).done!
+  end
+
+  def mark_undone_at(index)
+    @todos.fetch(index).undone!
+  end
+
+  def done!
+    @todos.map { |item| item.done! }
+  end
+
+  def shift
+    @todos.shift
+  end
+
+  def pop
+    @todos.pop
+  end
+
+  def remove_at(index)
+    @todos.delete(@todos.fetch(index))
+  end
+
+  def to_s
+    "---- Today's Todos ----" + "\n" +
+    @todos.map { |item| item.to_s }.join("\n")
+  end
+
+  def each
+    counter = 0
+    while counter < @todos.size
+      yield(@todos[counter])
+      counter += 1
+    end
+    self
+  end
+
+  def select
+    selected = TodoList.new(title)
+    each do |item|
+      selected << item if yield(item)
+    end
+    selected
+  end
+
+  def find_by_title(search_term)
+    select { |todo| todo.title == search_term }.first
+  end
+
+  def all_done
+    select { |todo| todo.done? }
+  end
+
+  def all_not_done
+    select { |todo| !todo.done? }
+  end
+
+  def mark_done(search_term)
+    find_by_title(search_term) && find_by_title(search_term).done!
+  end
+
+  def mark_all_done
+    each { |todo| todo.done! }
+  end
+
+  def mark_all_undone
+    each { |todo| todo.undone! }
+  end
+
+end
+
+todo1 = Todo.new("Buy milk")
+todo2 = Todo.new("Clean room")
+todo3 = Todo.new("Go to gym")
+
+list = TodoList.new("Today's Todos")
+list.add(todo1)
+list.add(todo2)
+list.add(todo3)
